cfa17e0bf5ddbdf25817691eb4f0de7efe705520
Edward Jin <57547638+EddieJ03@users.noreply.github.com>
2024-11-25 22:04:08 -0800

    setup azure container registry

--- index.ts
+++ index.ts
@@ -217,3 +217,28 @@ new TraefikRoute(
   },
   { provider: k8sprovider }
 );
+
+
+// Create an Azure Container Registry
+const registry = new azure.containerregistry.Registry("mlregistry", {
+  resourceGroupName: resourceGroup.name,
+  sku: {
+      name: "Basic",
+  },
+  adminUserEnabled: true,
+  location: resourceGroup.location,
+});
+
+const resourceGroupName = resourceGroup.name;
+
+// Get the admin credentials for the Azure Container Registry
+const credentials = pulumi.all([resourceGroup.name, registry.name]).apply(([resourceGroupName, registryName]) => 
+  azure.containerregistry.listRegistryCredentials({
+      resourceGroupName: resourceGroupName,
+      registryName: registryName,
+  })
+);
+
+export const adminUsername = credentials.apply(c => c.username!);
+export const adminPassword = credentials.apply(c => c.passwords![0].value!);
+export const registryUrl = registry.loginServer;
